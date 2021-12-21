995762b2a3af3216431b6164431a2df3aef52040
Gustavo-F <fgustavo016@gmail.com>
2021-12-21 14:48:09 -0300

    Added Person and User class. Config SpringFramework dependencies.

---
+++ src/main/java/com/bookshop/Entities/User.java
@@ -0,0 +1,17 @@
+package com.bookshop.Entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User extends Person{
+    private String username;
+    private String password;
+    private List<Book> library;
+
+    public void addBook(Book book) {
+        if (this.library == null)
+            this.library = new ArrayList<Book>();
+
+        this.library.add(book);
+    }
+}
